5977437e0c5c006575f3d0f470bdaaa9c570ad21
daviport <daviportdev@gmail.com>
2024-05-30 02:49:08 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-// import { useState } from 'react'
-// import './App.css';
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
-import TodoApp from './components/Tasks/Tasks';
+import Todo from './components/ToDo/Todo';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/task" element={<TodoApp />} />
+      <Route path="/todo" element={<Todo />} />
     </Routes>
   );
 };
